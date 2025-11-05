11e392df6266066cea96aba971dc0b4ca42f70a1
Nathandbrl <nathan.dubreuil07@gmail.com>
2025-11-05 18:19:42 +0100

    final

--- model/bdd.php
+++ model/bdd.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $pdo = new PDO("pgsql:host=localhost;dbname=smartbike;port=5432", "postgres", "Vaillant");
+    $pdo = new PDO("pgsql:host=localhost;dbname=smartbike;port=5432", "postgres", "Chanel01+");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erreur de connexion à la base de données : " . $e->getMessage());
