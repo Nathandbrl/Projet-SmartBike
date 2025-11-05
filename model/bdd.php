860567d9ea109c975076e42e90791b5e7fd191e3
Aden Fidon <aden.fidon@tumo.world>
2025-11-05 17:24:36 +0100

    Contacts ajouter

--- model/bdd.php
+++ model/bdd.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $pdo = new PDO("pgsql:host=localhost;dbname=smartbike", "postgres", "Chanel01+");
+    $pdo = new PDO("pgsql:host=localhost;dbname=smartbike", "postgres", "Vaillant");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erreur de connexion à la base de données : " . $e->getMessage());
