c4cbc5bc122204d173bf65e14e2d973a4aa6ed26
Landry <landry.pihour@efrei.net>
2025-11-05 17:33:50 +0100

    page contact

---
+++ view/contact.php
@@ -0,0 +1,15 @@
+<h1>Contactez-nous</h1>
+
+        <form action="mailto:tonemail@example.com" method="post" enctype="text/plain">
+            <label for="nom">Nom:</label>
+            <input type="text" id="nom" name="nom" required>
+            <label for="prénom">Nom:</label>
+            <input type="text" id="prénom" name="prénom" required>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+            <label for="message">Message:</label>
+            <textarea id="message" name="message" required></textarea>
+            <button type="submit">Envoyer</button>
+        </form>
+
+    
